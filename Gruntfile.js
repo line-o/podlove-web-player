3b70f416902c8cd7bef0a338d0044b5d4fe1b1c2
Juri Leino <github@line-o.de>
2013-09-25 21:42:42 +0200

    further restructuring
    Wordpress plugin files now in folder `wp-player-plugin`.
    Added task `grunt watch`for SASS and JS files.
    Compress and minify on `grunt build`.
    Moved all files and folders from `build` to project root.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,95 @@
 module.exports = function(grunt) {
 
+    var pwpBanner = '/*! ==========================================\n' +
+        ' * <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+        ' * <%= pkg.author %>\n' +
+        ' * <%= pkg.repository.url %>\n' +
+        ' * Licensed under The BSD 2-Clause License\n' +
+        ' * http://opensource.org/licenses/BSD-2-Clause\n' +
+        ' * =========================================== */\n';
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            all: ['Gruntfile.js', 'podlove-web-player/**/*.js'],
+            all: ['Gruntfile.js', 'js/*.js'],
             options: {
                 ignores: [
-                    'podlove-web-player/libs/jquery-1.9.1.min.js',
-                    'podlove-web-player/libs/jquery.ba-hashchange.min.js',
-                    'podlove-web-player/libs/html5shiv.js'
+                    'js/podlove-web-player.min.js',
+                    'js/lib/hashchange.min.js',
+                    'js/lib/zepto.js'
                 ]
             }
         },
         uglify: {
             options: {
-                banner: '/*! ==========================================\n' +
-                        ' * <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-                        ' * <%= pkg.author %>\n' +
-                        ' * <%= pkg.repository.url %>\n' +
-                        ' * Licensed under The BSD 2-Clause License\n' +
-                        ' * http://opensource.org/licenses/BSD-2-Clause\n' +
-                        ' * =========================================== */\n'
+                banner: pwpBanner
             },
             build: {
-                src: 'podlove-web-player/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'js/<%= pkg.name %>.min.js'
             }
         },
         sass: {                              // Task
             dist: {                            // Target
                 options: {                       // Target options
-                    style: 'expanded'
+                    style: 'compressed',
+                    compass: true,
+                    banner: pwpBanner
                 },
                 files: {
-                    "build/css/podlove-web-player.css": "podlove-web-player/css/podlove-web-player.scss"
-                }
-            }
-        }
-        /*
-        less: {
-            development: {
-                options: {
-                    paths: ["podlove-web-player/css"]
-                },
-                files: {
-                    "podlove-web-player/podlove-web-player.css": "podlove-web-player/podlove-web-player.less"
+                    "css/pwp.css": "sass/podlove-web-player.scss"
                 }
             },
-            production: {
-                options: {
-                    paths: ["podlove-web-player/css"],
-                        yuicompress: true
+            dev: {
+                options: {                       // Target options
+                    style: 'expanded',
+                    compass: true,
+                    lineNumbers: true
                 },
                 files: {
-                    "build/pwp.css": "podlove-web-player/podlove-web-player.less"
+                    "css/pwp.css": "sass/podlove-web-player.scss"
                 }
             }
         },
-        */
+        watch: {
+            options: {
+                dateFormat: function(time) {
+                    grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+                    grunt.log.writeln('Waiting for more changes...');
+                },
+                livereload: true
+            },
+            sass: {
+                files: ['sass/*.scss'],
+                tasks: ['sass:dev']
+            },
+            /* watch and see if our javascript files change, or new packages are installed */
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    spawn: false
+                }
+            },
+            /* watch our files for change, reload
+            livereload: {
+                files: ['sass/*.scss', 'js/podlove-web-player.js'],
+                options: {
+                    livereload: true
+                }
+            }
+            * */
+        }
     });
 
     // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('build', ['jshint', 'sass', 'uglify']);
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('build', ['jshint', 'uglify', 'sass:dist']);
+    grunt.registerTask('default', ['build']);
 };
